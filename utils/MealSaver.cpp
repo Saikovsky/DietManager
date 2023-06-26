d1afa65457b7cf4c16eced4ceae7a16b647c9787
A B <latweid2@yahoo.com>
2023-06-26 08:54:33 +0200

    Fix MealSaver and prepare for saving function

--- utils/MealSaver.cpp
+++ utils/MealSaver.cpp
@@ -26,21 +26,26 @@ MealSaver::MealSaver()
     meal_builder.add_meal_type(mealType);
     auto meal = meal_builder.Finish();
 
-    //builder.Finish(meal);
-    builder.Finish(meal);
+    std::vector<flatbuffers::Offset<Meals::Meal>> meal_vector;
+    meal_vector.push_back(meal);
+    auto meals = builder.CreateVector(meal_vector);
+    //Meals::MealsBuilder meals_builder(builder);
+    auto mealsb = Meals::CreateMeals(builder, meals);
+
+    builder.Finish(mealsb);
 
     uint8_t* buf = builder.GetBufferPointer();
     int size = builder.GetSize();
     // Get a pointer to the root object inside the buffer.
-    //auto mealss = Meals::GetMeals(buf);
+    auto mealss = Meals::GetMeals(buf);
     
     //std::ofstream ofile("bin/data.bin", std::ios::binary);
     //ofile.write((char*)buf, size);
     //ofile.close();
 
     Logger log;
-    //auto nameToLog = mealss->meals()->begin()->description();
+    auto nameToLog = mealss->meals()->begin()->description();
     
-    std::string strtolog = std::to_string((uint8_t)ok);
+    std::string strtolog = std::string(nameToLog->c_str());
     log.info("nameToLog"+ strtolog);
 };
